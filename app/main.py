fb4849ad218f29d8daf88d06f2b4d0e24deea1b8
Zeugma324 <noam.pinard.1@ulaval.ca>
2024-12-02 05:26:36 -0500

    POST cuisinier

--- app/main.py
+++ app/main.py
@@ -5,8 +5,8 @@ Il contient également les requêtes GET.
 """
 
 from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
-from typing import List
+from pydantic import BaseModel, RootModel, Field
+from typing import List, Optional, Dict
 from app import (
     recipes_collection,
     con,
@@ -136,6 +136,7 @@ class RecipesResponse(BaseModel):
     nom: str
     ingredients: List[str]
     description: str
+    #tags: List[str]
 
 @app.post("/recette", response_model=RecipesResponse)
 async def get_recette(request: RecipesRequest):
@@ -143,15 +144,171 @@ async def get_recette(request: RecipesRequest):
 
     query = {}
     if types_recherche:
-        query["tags"] = {"$in" : types_recherche}
+        query["tags"] = {"$all" : types_recherche}
         
-        recette = recipes_collection.find_one(query, {"name": 1, "ingredients": 1, "description": 1})
+    pipeline = [
+        {"$match" : query},
+        {"$sample" : {"size" : 1}},
+        {"$project" : {"name" : 1, "ingredients":  1, "description": 1, "tags" : 1}}
+    ]
 
-        if not recette:
-            raise HTTPException(status_code=404, detail="Aucune recette trouvée pour les types donnés.")
+    recette_list = list(recipes_collection.aggregate(pipeline))
+    recette = recette_list[0]
 
-        return RecipesResponse(
-            nom=recette["name"],
-            ingredients=recette["ingredients"],
-            description=recette.get("description", "Aucune description disponible.")
-        )
+    if not recette:
+        raise HTTPException(status_code=404, detail="Aucune recette trouvée pour les types donnés.")
+    
+    return RecipesResponse(
+        nom=recette["name"],
+        ingredients=recette["ingredients"],
+        description=recette.get("description", "Aucune description disponible."),
+        #tags=recette["tags"]
+    )
+
+class recette(BaseModel):
+    nomProduit: str = Field(alias="nom")
+    ingrédients: List[str]
+    description: Optional[str]
+
+class indicateursDeQualite_1(BaseModel):
+    Nutriscore: Optional[str]
+    Nova: Optional[str]
+    Ecoscore: Optional[str]
+
+class CuisinerRequest(BaseModel):
+    recette: recette
+    preferenceMarqueProduit: Optional[str] = None
+    indicateursDeQualiteSuperieurA: Optional[indicateursDeQualite_1] = None
+
+class RecoProduit(BaseModel):
+    nomProduit: str
+    indicateursDeQualite: Optional[indicateursDeQualite_1] = None
+    marque: Optional[str] = None
+    categories: Optional[List[str]] = None
+    produitDeBase: bool
+
+class CuisinerResponse(BaseModel):
+    data: Dict[str, List[RecoProduit]]
+
+def query_constructor(request, ing):
+
+    recette = request.recette
+    preference = request.preferenceMarqueProduit
+    indicateurs = request.indicateursDeQualiteSuperieurA
+
+    query = f"""SELECT product_name, ecoscore_grade, nutriscore_grade, nova_groups, brands_tags, categories
+        FROM '{PARQUET_FILE}' WHERE product_name LIKE '%{ing}%'"""
+
+    if indicateurs and indicateurs.Ecoscore:
+        ecoscore_values = ', '.join(f"'{e}'" for e in indicateurs.Ecoscore)
+        ecoscore_clause = f" AND ecoscore_grade IN ({ecoscore_values})"
+        query += ecoscore_clause
+    else:
+        query += " AND ecoscore_grade IS NOT NULL"
+
+    if indicateurs and indicateurs.Nutriscore:
+        nutriscore_values = ', '.join(f"'{n}'" for n in indicateurs.Nutriscore)
+        nutriscore_clause = f" AND nutriscore_grade IN ({nutriscore_values})"
+        query += nutriscore_clause
+    else:
+        query += " AND nutriscore_grade IS NOT NULL"
+
+    if indicateurs and indicateurs.Nova:
+        nova_values = ', '.join(str(n) for n in indicateurs.Nova)
+        nova_clause = f" AND nova_groups IN ({nova_values})"
+        query += nova_clause
+    else:
+        query += " AND nova_groups IS NOT NULL"
+
+    if preference:
+        preference_clause = f""" AND EXISTS (
+            SELECT *
+            FROM UNNEST(brands_tags) AS brand
+            WHERE CAST(brand AS VARCHAR) LIKE '%{preference}%'
+            )"""
+        query += preference_clause
+    else:
+        query += " AND brands_tags IS NOT NULL"
+
+    query += " LIMIT 1"
+
+    print("Generated Query:")
+    print(query)
+
+    return query
+
+
+@app.post("/cuisiner", response_model=CuisinerResponse)
+async def get_cuisiner(request: CuisinerRequest):
+    recette = request.recette
+    preference = request.preferenceMarqueProduit
+    indicateurs = request.indicateursDeQualiteSuperieurA
+
+    mysql_conn = create_connection(
+        "ingredients", "api_endpoint", "abracadabra", "raw_ingredients"
+    )
+    
+    response = {}
+    for i in recette.ingrédients:
+        
+        base_aliment = False
+
+        if mysql_conn:
+            base_query = f"""SELECT FoodDescription FROM food_name WHERE EstAlimentDeBase = 1 AND FoodDescription LIKE '{i}%' LIMIT 1;"""
+            print("sql :" ,i)
+            cursor = mysql_conn.cursor()
+            cursor.execute(base_query)
+            base = cursor.fetchall()
+            
+        
+        if base:
+            for row in base:
+                base = row[0]
+                base_aliment = True
+
+        if not base_aliment:
+            query = query_constructor(request, i)
+            results = con.execute(query).fetchall()
+            print("COMO ESTA")
+            print(results)
+        
+        if i not in response:
+            response[i] = []
+
+
+        if base_aliment:
+            response[i].append(
+                RecoProduit(
+                    nomProduit=base,
+                    indicateursDeQualite=None,
+                    marque=None,
+                    categories=None,
+                    produitDeBase=True
+                )
+            )
+        else:
+            for row in results:
+                product_name, ecoscore, nutriscore, nova, brands, categories = row
+
+                if isinstance(categories, str):
+                    categories = categories.split(", ")
+                
+                brands = brands[0]
+
+                response[i].append(
+                    RecoProduit(
+                        nomProduit=product_name,
+                        indicateursDeQualite=indicateursDeQualite_1(
+                            Nutriscore=nutriscore or None,
+                            Nova=nova or None,
+                            Ecoscore=ecoscore or None
+                        ),
+                        marque=brands or None,
+                        categories=categories or None,
+                        produitDeBase=False
+                    )
+                )
+        base = []
+    cursor.close()
+    mysql_conn.close()
+    return CuisinerResponse(data=response)
